39731d842b7a4fbc7e6e8f4319a3e3a0b6150943
Dmitrii <onolite@gmail.com>
2020-10-05 17:57:28 +0300

    task6

--- src/main/java/com/owpk/hw/entities/Product.java
+++ src/main/java/com/owpk/hw/entities/Product.java
@@ -5,18 +5,29 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
-@NoArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 @Table(name = "products")
 public class Product {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String title;
   private Integer price;
+
+  @OneToMany(mappedBy = "product")
+  List<OrderItem> orderItem;
+
+  @Override
+  public String toString() {
+    return "Product{" + "ID" + id +
+        "title='" + title + '\'' +
+        ", price=" + price +
+        '}';
+  }
 }
-
-
