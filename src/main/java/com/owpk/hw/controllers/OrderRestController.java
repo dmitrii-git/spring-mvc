8ae2030e88b9630e7dba92dd4908b44b811d4df7
Dmitrii <onolite@gmail.com>
2020-11-27 16:31:33 +0300

    Spring Integration

--- src/main/java/com/owpk/hw/controllers/OrderRestController.java
+++
@@ -1,9 +0,0 @@
-package com.owpk.hw.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/v1/orders")
-public class OrderRestController {
-}
